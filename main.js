334a37a9836faf7ec301cb0ce52d5a7548de3a61
Jacobtread <jacobtread@gmail.com>
2022-02-01 21:23:14 +1300

    App source code

--- main.js
+++ main.js
@@ -1,29 +1,80 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, screen, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+const fs = require('fs')
+const storageDir = path.join(process.env.APPDATA, 'Hour.ly')
+const storageFile = path.join(storageDir, 'times.json')
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 400,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+function createWindow() {
+    // Create the browser window.
+    let display = screen.getPrimaryDisplay();
+    let width = display.bounds.width;
+    let height = display.bounds.height;
+    const mainWindow = new BrowserWindow({
+        width: 160,
+        height: 200,
+        titleBarStyle: 'hidden',
+        resizable: false,
+        transparent: true,
+        alwaysOnTop: true,
+        x: width - 225,
+        y: height - 205,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    })
 
-  mainWindow.loadFile('index.html').then().catch();
+    mainWindow.loadFile('index.html').then().catch();
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
 
+ipcMain.on('minimize', () => {
+    BrowserWindow.getFocusedWindow().minimize()
+})
+ipcMain.on('close', () => {
+    BrowserWindow.getFocusedWindow().close()
+})
+
+ipcMain.on('save', (event, args) => {
+    if (!fs.existsSync(storageDir)) {
+        fs.mkdirSync(storageDir)
+    }
+    let total = 0
+    for (let arg of args) {
+        total += arg
+    }
+    const data = JSON.stringify({
+        history: args,
+        total
+    })
+    fs.writeFileSync(storageFile, data, 'utf8')
+})
+
+ipcMain.on('load', function (event) {
+    loadData(event)
+})
+
+function loadData(event) {
+    if (fs.existsSync(storageFile)) {
+        try {
+            const data = fs.readFileSync(storageFile, 'utf8')
+            const json = JSON.parse(data)
+            if (json.history) {
+                event.sender.send('load-state', json.history)
+            }
+        } catch (e) {
+        }
+    }
+}
--- preload.js
+++ preload.js
@@ -1,12 +1,74 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+const { ipcRenderer } = require('electron')
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('minimize').onclick = () => {
+        ipcRenderer.send('minimize')
+    }
+    document.getElementById('close').onclick = () => {
+        ipcRenderer.send('close')
+    }
+
+    const actionButton = document.getElementById('action');
+    const elapsedTime = document.getElementById('elapsed');
+    const historyElm = document.getElementById('history');
+    const total = document.getElementById('total');
+
+    let started = false;
+    let startedAt = 0
+    let history = []
+
+    document.getElementById('action').onclick = function () {
+        started = !started
+        if (started) {
+            actionButton.classList.add('started')
+            startedAt = performance.now()
+        } else {
+            actionButton.classList.remove('started')
+            const time = performance.now()
+            const diff = time - startedAt
+            history.push(diff)
+            updateHistory()
+        }
+    }
+
+    function updateHistory() {
+        historyElm.innerHTML = ''
+        let totalTime = 0
+        for (let i = history.length - 1; i >= 0; i--) {
+            const element = history[i];
+            const html = document.createElement('li')
+            html.innerText = getPrettyValue(element)
+            historyElm.appendChild(html)
+            totalTime += element
+        }
+        total.innerText = getPrettyValue(totalTime)
+        ipcRenderer.send('save', history)
+    }
+
+    ipcRenderer.send('load')
+
+    ipcRenderer.on('load-state', function (event, args) {
+        history = args
+        updateHistory()
+    })
+
+    function getPrettyValue(value) {
+        if (value / 3600000 > 1) /* hours */ {
+            return (value / 3600000).toFixed(1) + 'h'
+        } else if (value / 60000 > 1) /* minutes */{
+            return (value / 60000).toFixed(0) + 'm'
+        } else /* seconds*/ {
+            return (value / 1000).toFixed(0) + 's'
+        }
+    }
+
+    setInterval(() => {
+        if (started) {
+            const time = performance.now()
+            const passed = time - startedAt
+            elapsedTime.innerText = getPrettyValue(passed)
+        }
+    }, 100)
 })
